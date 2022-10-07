8771f6eb70a4bba3e3db5fc43f084f1f3e566af9
Aman Sharma <78209498+sage-arts@users.noreply.github.com>
2022-10-07 10:30:25 +0530

    Update test.cpp
    uniform syntax

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
-#include "stdlib.h"
-#include "iostream"
-#include "math.h"
-#include "omp.h"
+#include <stdlib.h>
+#include <iostream>
+#include <math.h>
+#include <omp.h>
 #include <fstream>
 #include <string>
 #include <vector>
